13e34856976b254881d06ca6d45591a25577bf07
Samuel <Samuel_SFlores@hotmail.com>
2023-02-22 21:51:00 -0600

    Start template

--- src/components/evoluciones/components/nuevaEvolucionForm.jsx
+++ src/components/evoluciones/components/nuevaEvolucionForm.jsx
@@ -4,13 +4,30 @@ import { Button } from 'primereact/button'
 
 export const NuevaEvolucionForm = () => {
 
-    const [value, setValue] = useState('')
+    const [evolucion, setEvolucion] = useState('');
+    const [idEvolucion, setIdEvolucion] = useState('');
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
 
     return (
-        <div>
-            <InputText value={value} onChange={(e) => setValue(e.target.value)} />
+        <>
+            <label htmlFor="idEvolucion">Id Evolución</label>
+            <InputText
+                className='inputRed'
+                name='idEvolucion'
+                value={idEvolucion}
+                onChange={(e) => setIdEvolucion(e.target.value)}
+            />
 
-            <Button label="Click" icon="pi pi-check" />
-        </div>
+            <label htmlFor="evolucion">Evolución</label>
+            <InputText
+                name='evolucion'
+                value={evolucion}
+                onChange={(e) => setEvolucion(e.target.value)}
+            />
+            <Button label="Click" icon="pi pi-check" type='button' onClick={() => onSubmit({ evolucion, idEvolucion })} />
+        </>
     )
 }
